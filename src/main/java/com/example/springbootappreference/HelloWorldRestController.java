77d289db0d3d9fe8206b7cd14699deffcc533d18
Edmilson Prata <edprata@gmail.com>
2021-01-10 19:28:09 -0300

    ajustes

--- src/main/java/com/example/springbootappreference/HelloWorldRestController.java
+++ src/main/java/com/example/springbootappreference/HelloWorldRestController.java
@@ -1,22 +1,22 @@
-package com.example.demo;
+package com.example.springbootappreference;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@EnableAutoConfiguration
 public class HelloWorldRestController {
 
 	private static Long count = 1L;
 
-	@GetMapping("/api/hello")
+	@RequestMapping("/api/hello")
 	public String index(String name) {
 		return "Hello World!!!";
 	}
 
 	@GetMapping("/api/helloWithName")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+	public String hello(@RequestParam(value = "name", defaultValue = "No Named") String name) {
 		return String.format("Hello %s!", name);
 	}
 

c18c10eeb302a69a7f7dadc32edf3ab5dbb957d3
Edmilson Prata <edprata@gmail.com>
2021-01-10 19:03:17 -0300

    ajustes

--- src/main/java/com/example/springbootappreference/SpringBootAppReference.java
+++ src/main/java/com/example/springbootappreference/SpringBootAppReference.java
@@ -2,12 +2,20 @@ package com.example.springbootappreference;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class SpringBootAppReference {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootAppReference.class, args);
+	}
+
+	@GetMapping("/")
+	public String index(String name) {
+		return "Hello World!!!";
 	}
 
 }

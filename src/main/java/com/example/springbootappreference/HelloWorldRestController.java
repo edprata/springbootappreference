d4be635478a5a783b3ba09d6d14187083aed3cf5
Edmilson Prata <edprata@gmail.com>
2021-01-10 19:11:26 -0300

    ajustes

--- src/main/java/com/example/springbootappreference/HelloWorldRestController.java
+++ src/main/java/com/example/springbootappreference/HelloWorldRestController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@EnableAutoConfiguration
 public class HelloWorldRestController {
 
 	private static Long count = 1L;
